bdecba3295f3d69631829de9c308bea86949aa6f
marcosdayanm <marcosdayanmann@gmail.com>
2024-03-29 01:10:43 -0600

    First steps into Next.js

---
+++ app/ui/fonts.ts
@@ -0,0 +1,11 @@
+import { Inter, Lusitana, Comic_Neue } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+export const comic_neue = Comic_Neue({
+  subsets: ['latin'],
+  weight: '300',
+});
+export const lusitana = Lusitana({
+  subsets: ['latin'],
+  weight: '400',
+});
